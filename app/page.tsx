7ae741a12d37b7b293645ee7351ff2b756d4825f
Adrian Pizarro Serrano <adrianpizarroserrano@hotmail.com>
2024-12-29 20:42:59 +0100

    fix: texts and improvements

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { HotelCard } from './components/HotelCard'
 export default function Home() {
   return (
     <main className="min-h-screen bg-white" style={{ fontFamily: 'Montserrat' }}>
-      {/* Hero Section */}
+      {/* Main Section */}
       <section className="relative h-screen flex items-center justify-center text-center px-4">
       <div className="absolute inset-0 bg-[url('/img/background.png')] bg-cover bg-center" />
         <div className="relative z-10 space-y-6">
@@ -23,9 +23,9 @@ export default function Home() {
           <p className="text-lg md:text-xl text-gray-600 flex flex-col md:flex-row justify-center items-center space-y-2 md:space-y-0 md:space-x-2 text-center">
             <a href="https://g.co/kgs/J7js8C8" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-1" style={{ color: '#787954' }}>
               <MapPin style={{ color: '#787954' }}/>
-              <span>Soto de Cerrolen, Madrid</span>
+              <span>Soto de Cerrolén, Madrid</span>
             </a>
-            <span  style={{ color: '#787954' }}>19.00h</span>
+            <span  style={{ color: '#787954' }}>19:00h</span>
           </p>
         </div>
       </section>
@@ -33,7 +33,7 @@ export default function Home() {
       {/* RSVP Section */}
       <section className="py-20 px-4 bg-gray-50 relative overflow-hidden">        
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl md:text-4xl font-serif mb-8 text-gray-800">Confirma tu asistencia</h2>
+          <h2 className="text-3xl md:text-4xl font-serif mb-8 text-gray-800">¿Nos acompañas en este día tan especial?</h2>
           <Link
             href="https://forms.gle/WAmEHXbeVCrBsrqz7"
             target='_blank'
@@ -51,7 +51,10 @@ export default function Home() {
           <div className="space-y-6">
             <h2 className="text-3xl md:text-4xl font-serif text-gray-800">Nuestra historia</h2>
             <p className="text-gray-600 leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Hace diez años, nuestras vidas se cruzaron por primera vez en la universidad, dentro de la asociación cultural La Hermandad. Lo que comenzó como una amistad sencilla, cargada de complicidad, poco a poco se transformó en algo mucho más profundo: un amor que no ha dejado de crecer.
+            </p>
+            <p className="text-gray-600 leading-relaxed">
+              Diez años después, con tantos momentos compartidos y recuerdos que nos han hecho más fuertes, seguimos descubriendo cada día nuevas razones para amarnos. Ahora, estamos listos para dar el siguiente paso en esta historia que, sin duda, aún tiene muchas páginas por escribir.
             </p>
           </div>
           <div className="relative h-[400px]">
@@ -80,9 +83,8 @@ export default function Home() {
               icon={<Heart className="w-6 h-6" />}
               time="19:00"
               title="Inicio de la ceremonia"
-              description="Soto de Cerrolen"
-            />
-            
+              description="Soto de Cerrolén"
+            />            
             <TimelineItem
               icon={<PartyPopper className="w-6 h-6" />}
               time="20:00"
@@ -91,7 +93,7 @@ export default function Home() {
             />
             <TimelineItem
               icon={<Bus className="w-6 h-6" />}
-              time="04:00"
+              time="05:00"
               title="Vuelta del autobús"
               description="Regreso a TBD"
             />
@@ -124,12 +126,11 @@ export default function Home() {
       </section>
 
       {/* Pets Section */}
-      <section className="py-20 px-4 bg-gray-50" style={{ marginBottom: '0px', paddingBottom: '0px'}}
-      >
+      <section className="py-20 px-4 bg-gray-50" style={{ marginBottom: '0px', paddingBottom: '0px'}}>
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl md:text-4xl font-serif mb-8 text-gray-800">Mascotas</h2>
+          <h2 className="text-3xl md:text-4xl font-serif mb-8 text-gray-800">Momento mascota</h2>
           <p className="text-gray-600 mb-8">
-            Sabemos que las echarás de menos a tus mascotas, por eso queremos que de alguna manera estén presentes.
+            Sube una foto de tu compañero más fiel y déjate sorprender el día de nuestra boda.
           </p>
           <Link
             href="https://forms.gle/myXcrymWJpdaj7by5"
@@ -139,23 +140,23 @@ export default function Home() {
           >
             ¡Sube una foto!
           </Link>
-          <div className="relative h-[125px]">
-          <Image
+          <div className="relative w-full" style={{ height: '200px', overflow: 'hidden' }}>
+            <Image
               src="/img/tina.png"
               fill
               alt="tina"
-              className="object-contain rounded-lg"
+              className="object-cover rounded-lg"
             />
-          </div>          
+          </div>        
         </div>
       </section>
 
       {/* Gift Registry Section */}
       <section className="py-20 px-4">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl md:text-4xl font-serif mb-8 text-gray-800">Nuestro viaje</h2>
+          <h2 className="text-3xl md:text-4xl font-serif mb-8 text-gray-800">Próxima parada: Japón</h2>
           <p className="text-gray-600 mb-8">
-            Tu presencia en nuestra boda es el mejor regalo de todos. Sin embargo, si deseas ayudarnos con nuestra luna de miel, puedes contribuir en esta cuenta:
+            Tu presencia en nuestra boda es el mejor regalo que podemos recibir. Si quieres hacernos un regalo extra para nuestra luna de miel, puedes contribuir en esta cuenta:
           </p>
           <div className="bg-gray-50 p-6 rounded-lg inline-block">
             <p className="font-mono text-gray-800">ES74 0182 1294 1002 0293 0217</p>
@@ -168,7 +169,7 @@ export default function Home() {
         {/* Content Layer */}
         <div className="relative z-10 max-w-4xl mx-auto text-center">
           <h2 className="text-3xl md:text-4xl font-serif mb-8 text-gray-800">
-            ¡Gracias por ser parte de nuestro día especial!
+            ¡Te esperamos! 
           </h2>
           <p className="text-gray-600 mb-8">
             Si tienes alguna duda, por favor no dudes en contactarnos:
